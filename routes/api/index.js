f257592bd63f61a8bbdfa93319c412f893429bc1
Ayberk Tandogan <ayberk.tandogan@outlook.com>
2020-02-18 23:53:07 +0300

    Sonunda Versiyon Kontrolü?

--- routes/api/index.js
+++ routes/api/index.js
@@ -34,7 +34,7 @@ router.get('/', (req, res) => {
     const response = {
         author: 'aybertocarlos',
         contact: 'aybertocarlos@gmail.com',
-        version: '2.2.1.0',
+        version: '2.2.2',
         status: 'OK',
     }
     res.status(200).json(response)
