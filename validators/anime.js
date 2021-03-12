db84f530e7eb7cb853ee683a7a714fcc75088719
ayberktandogan <ayberk.tandogan@outlook.com>
2021-03-13 01:29:44 +0300

    validator bug fixed

--- validators/anime.js
+++ validators/anime.js
@@ -1,24 +1,41 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const addAnimeSchema = Joi.object({
     name: Joi.string().max(100).required(),
     synopsis: Joi.string().max(65535).required(),
     translators: Joi.string().max(255).required(),
     encoders: Joi.string().max(255).required(),
-    series_status: Joi.string().valid("currently_airing", "finished_airing", "not_aired_yet", "postponed", "canceled").required(),
-    trans_status: Joi.string().valid("currently_airing", "finished_airing", "not_aired_yet", "postponed", "canceled").required(),
+    series_status: Joi.string()
+        .valid(
+            "currently_airing",
+            "finished_airing",
+            "not_aired_yet",
+            "postponed",
+            "canceled"
+        )
+        .required(),
+    trans_status: Joi.string()
+        .valid(
+            "currently_airing",
+            "finished_airing",
+            "not_aired_yet",
+            "postponed",
+            "canceled"
+        )
+        .required(),
     release_date: Joi.date().required(),
     episode_count: Joi.number().required(),
     studios: Joi.string().max(255).required(),
     cover_art: Joi.string().uri().max(255).required(),
-    logo: Joi.string().uri().optional().allow(''),
-    header: Joi.string().uri().optional().allow(''),
+    logo: Joi.string().uri().optional().allow(""),
+    header: Joi.string().uri().optional().allow(""),
     mal_link: Joi.string().uri().max(100).required(),
     genres: Joi.string().max(255).required(),
-    premiered: Joi.string().max(50).optional().allow(''),
+    premiered: Joi.string().max(50).optional().allow(""),
     version: Joi.string().valid("bd", "tv").required(),
-    pv: Joi.string().uri().max(255).required()
-})
+    pv: Joi.string().uri().max(255).required(),
+    slug: Joi.string().allow(""),
+});
 
 const updateAnimeSchema = Joi.object({
     id: Joi.number().required(),
@@ -26,30 +43,52 @@ const updateAnimeSchema = Joi.object({
     synopsis: Joi.string().max(65535).required(),
     translators: Joi.string().max(255).required(),
     encoders: Joi.string().max(255).required(),
-    series_status: Joi.string().valid("currently_airing", "finished_airing", "not_aired_yet", "postponed", "canceled").required(),
-    trans_status: Joi.string().valid("currently_airing", "finished_airing", "not_aired_yet", "postponed", "canceled").required(),
+    series_status: Joi.string()
+        .valid(
+            "currently_airing",
+            "finished_airing",
+            "not_aired_yet",
+            "postponed",
+            "canceled"
+        )
+        .required(),
+    trans_status: Joi.string()
+        .valid(
+            "currently_airing",
+            "finished_airing",
+            "not_aired_yet",
+            "postponed",
+            "canceled"
+        )
+        .required(),
     release_date: Joi.date().required(),
     episode_count: Joi.number().required(),
     studios: Joi.string().max(255).required(),
     cover_art: Joi.string().uri().max(255).required(),
-    logo: Joi.string().uri().max(255).optional().allow(''),
-    header: Joi.string().uri().max(255).optional().allow(''),
+    logo: Joi.string().uri().max(255).optional().allow(""),
+    header: Joi.string().uri().max(255).optional().allow(""),
     mal_link: Joi.string().uri().max(100).required(),
     genres: Joi.string().max(255).required(),
-    premiered: Joi.string().max(50).optional().allow(''),
+    premiered: Joi.string().max(50).optional().allow(""),
     version: Joi.string().valid("bd", "tv").required(),
-    pv: Joi.string().uri().max(255).required()
-})
+    pv: Joi.string().uri().max(255).required(),
+    slug: Joi.string().allow(""),
+});
 
 const deleteAnimeSchema = Joi.object({
-    id: Joi.number().required()
-})
+    id: Joi.number().required(),
+});
 
 const updateFeaturedAnimeSchema = Joi.object({
     data: Joi.array().items({
         name: Joi.string().required(),
-        version: Joi.string().required()
-    })
-})
+        version: Joi.string().required(),
+    }),
+});
 
-module.exports = { addAnimeSchema, updateAnimeSchema, deleteAnimeSchema, updateFeaturedAnimeSchema }
+module.exports = {
+    addAnimeSchema,
+    updateAnimeSchema,
+    deleteAnimeSchema,
+    updateFeaturedAnimeSchema,
+};
